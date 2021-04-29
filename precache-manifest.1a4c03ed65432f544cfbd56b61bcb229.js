13c23dfdc4a35150cfe7d187d8fa7ae9468887cc
anandhu-1942 <83400653+anandhu-1942@users.noreply.github.com>
2021-04-29 17:06:19 +0530

    Add files via upload

---
+++ precache-manifest.1a4c03ed65432f544cfbd56b61bcb229.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "81690576654dd31cf0ba",
+    "url": "/static/css/main.c703114a.chunk.css"
+  },
+  {
+    "revision": "81690576654dd31cf0ba",
+    "url": "/static/js/main.81690576.chunk.js"
+  },
+  {
+    "revision": "6eb6405c9d99258624bc",
+    "url": "/static/js/1.6eb6405c.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "ba92c57928f655a320169ca40f5648cb",
+    "url": "/index.html"
+  }
+];
